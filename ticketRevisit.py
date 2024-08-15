e97ceaa5cd068c1d108e14cdb040a7add3ec9d52
Akshat Jain <akshat0@yahoo.com>
2024-08-15 18:56:13 +0530

    cleap up

--- ticketRevisit.py
+++ ticketRevisit.py
@@ -1,5 +1,4 @@
 #  using gemini cookbook brista bots perspective
-
 import os
 from random import randint
 from typing import Iterable
@@ -15,9 +14,9 @@ genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 order = []  # The in-progress order.
 placed_order = []  # The confirmed, completed order.
 
-def add_to_order(drink: str, modifiers: Iterable[str] = ()) -> None:
+def add_to_order(ticket: str, modifiers: Iterable[str] = ()) -> None:
   """Adds the specified ticket to the customer's order, including any modifiers."""
-  order.append((drink, modifiers))
+  order.append((ticket, modifiers))
 
 
 def get_order() -> Iterable[tuple[str, Iterable[str]]]:
@@ -51,8 +50,8 @@ def confirm_order() -> str:
   if not order:
     print('  (no items)')
 
-  for drink, modifiers in order:
-    print(f'  {drink}')
+  for ticket, modifiers in order:
+    print(f'  {ticket}')
     if modifiers:
       print(f'   - {", ".join(modifiers)}')
 
@@ -60,9 +59,8 @@ def confirm_order() -> str:
 
 
 def place_order() -> int:
-  """Submit the order for further processing.
-
-  Returns:
+  """first enter the users detail in csv
+    Returns:
     processing the payment.
   """
   placed_order[:] = order.copy()
@@ -71,5 +69,38 @@ def place_order() -> int:
   # TODO(you!): Implement coffee fulfilment.
   return randint(1, 10)
 
-
 MUSEUM_BOT_PROMPT = open("bot_prompt.txt", "r").read()
+
+ordering_system = [add_to_order, get_order, remove_item, clear_order, confirm_order, place_order]
+
+# Toggle this to switch between Gemini 1.5 with a system instruction, or Gemini 1.0 Pro.
+use_sys_inst = False
+
+model_name = 'gemini-1.5-flash' if use_sys_inst else 'gemini-1.0-pro'
+
+if use_sys_inst:
+  model = genai.GenerativeModel(
+      model_name, tools=ordering_system, system_instruction=MUSEUM_BOT_PROMPT)
+  convo = model.start_chat(enable_automatic_function_calling=True)
+
+else:
+  model = genai.GenerativeModel(model_name, tools=ordering_system)
+  convo = model.start_chat(
+      history=[
+          {'role': 'user', 'parts': [MUSEUM_BOT_PROMPT]},
+          {'role': 'model', 'parts': ['OK I understand. I will do my best!']}
+        ],
+      enable_automatic_function_calling=True)
+
+
+@retry.Retry(initial=30)
+def send_message(message):
+  return convo.send_message(message)
+
+placed_order = []
+order = []
+
+
+while not placed_order:
+  response = send_message(input('> '))
+  print(response.text)
