a0dae7fbcb53ee72a57343366ce8b2155fda9b57
Akshat Jain <akshat0@yahoo.com>
2024-08-15 15:34:34 +0530

    created a  django folder

--- ticket.py
+++ ticket.py
@@ -9,27 +9,6 @@ from google.api_core import retry
 # modal api 
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
-# Create the model
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
-
-model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
-  generation_config=generation_config,
-  # safety_settings = Adjust safety settings
-  # See https://ai.google.dev/gemini-api/docs/safety-settings
-)
-
-chat_session = model.start_chat(
-  history=[
-  ]
-)
-
 
 # Visitor structure
 Visitor = Dict[str, str or int]
@@ -250,7 +229,7 @@ else:
   model = genai.GenerativeModel(model_name, tools=booking_system)
   convo = model.start_chat(
       history=[
-          {'role': 'user', 'parts': [MUSEUM_BOT_PROMPT]},
+          {'role': 'system', 'parts': [MUSEUM_BOT_PROMPT]},
           {'role': 'model', 'parts': ['OK I understand. I will do my best!']}
         ],
       enable_automatic_function_calling=True)
