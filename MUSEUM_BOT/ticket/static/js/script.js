6c85f44c7ec90b473c4a98f56e5cb440b60c8ed2
Manoj <manojpandawork@gmail.com>
2024-08-20 18:02:01 +0530

    styling the chat

--- MUSEUM_BOT/ticket/static/js/script.js
+++ MUSEUM_BOT/ticket/static/js/script.js
@@ -16,14 +16,17 @@ form.addEventListener("submit", (e) => {
       const res = JSON.parse(data.response)[0].your_response_back_to_user;
       console.info(data.response);
       main.innerHTML += ` 
-            <ul>
-                <li>
-                     <div class="user_input">${data.user_input}</div>
-                </li>
-                <li>
-                    <div class="response">${res}</div>
-               </li>
-            </ul>`
+<div class="chat-div">
+<div class="chat user_input">
+<label for="prompt"> You </label>
+<p name="prompt">${data.user_input}</p>
+</div>
+<div class="chat response">
+
+<label for="response"> Bot </label>
+<p name="response">${res}</p>
+</div>
+`
     })
     .catch((error) => {
 
