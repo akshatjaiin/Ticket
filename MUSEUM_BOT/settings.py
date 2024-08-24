48dba4ff44329bb2700ca50f91c32fa1362075b2
Manoj Panda <151429040+ManojPanda3@users.noreply.github.com>
2024-08-24 16:10:03 +0000

    update docker, ready for production

--- MUSEUM_BOT/settings.py
+++ MUSEUM_BOT/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
