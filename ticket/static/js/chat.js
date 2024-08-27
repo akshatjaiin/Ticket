5ee09bce65d4b9e730360a3bb603168afdef4b32
Manoj <manojpandawork@gmail.com>
2024-08-27 11:09:33 +0530

    try to fix

--- ticket/static/js/chat.js
+++ ticket/static/js/chat.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById("form-chat");
 const main = document.getElementById("main");
 const ticketViewer = document.getElementById("tickets");
@@ -46,11 +47,11 @@ const chatFetch = async (url, options) => {
   const chatDiv = document.createElement('div');
   chatDiv.classList.add("chat-div");
   main.appendChild(chatDiv);
-  console.log(form[1].value)
-  const userPrompt = form[1].value;
-  console.log("data", userPrompt)
-  chatDiv.innerHTML = `<div class="notranslate user_input" translate="no">
-      <p name="prompt" class="user_prompt"><span class="userSpanPrompt" translate="no">${userPrompt}</span></p>
+
+  const userPrompt = form[1].value.trim();
+  chatDiv.innerHTML = `
+    <div class="notranslate user_input" translate="no">
+      <p name="prompt" class=notranslate "user_prompt"><span class="userSpanPrompt" translate="no">${userPrompt}</span></p>
     </div>`;
 
   form[1].value = "";
@@ -145,7 +146,7 @@ const chatFetch = async (url, options) => {
       console.log(`Total price in USD: ${totalPrice}`);
 
       paypal.Buttons({
-        createOrder: function(data, actions) {
+        createOrder: function (data, actions) {
           return actions.order.create({
             purchase_units: [{
               amount: {
@@ -155,7 +156,7 @@ const chatFetch = async (url, options) => {
             }]
           });
         },
-        onApprove: async function(data, actions) {
+        onApprove: async function (data, actions) {
           console.log("Payment approved");
           const tickets = Object.keys(backendResponse.ticketDetails);
           console.log(tickets);
@@ -175,10 +176,10 @@ const chatFetch = async (url, options) => {
             console.error("Error processing payment: ", err);
           }
         },
-        onCancel: function(data, actions) {
+        onCancel: function (data, actions) {
           console.info("Payment cancelled");
         },
-        onError: function(err) {
+        onError: function (err) {
           console.error("Payment error: ", err);
           alert("Error while payment\n Error :", err)
         },
@@ -236,22 +237,17 @@ async function fetchTicket(ticketId) {
 
 function chat(e) {
   e.preventDefault(); // so that the page not reload multiple time on chat
-  if (!form[1].value) {
-    console.info("No input data found");
-    return;
-  }
-  console.log(form[1].value);
+  if (!form[1].value) return;
   const formData = new FormData(form);
   const url = "/";
   const options = {
     method: "POST",
     body: formData
   };
+  form.children[1].firstElementChild.firstChild.value = ""
   chatFetch(url, options);
 }
 form.addEventListener("submit", chat);
-
-<<<<<<< HEAD
 
 (() => {
   const formData = new FormData(form);
@@ -263,8 +259,6 @@ form.addEventListener("submit", chat);
                           I hate when someone asks more than one detail / question in at a time.
                           Just Age One by one no need to rush. 
                           I just want to know what you can do in a concise way.
-                          I might repeat the same prompt again and again,
-<<<<<<< HEAD
     just remind me if I do that and use different reminders each time.Todays Date is ${Date()}]`);
   const url = "/";
   const options = {
@@ -281,27 +275,6 @@ form.addEventListener("submit", chat);
         main.appendChild(chatDiv);
         chatDiv.innerHTML += `
       <div class="chat response" >
-<button class="text-to-speech" onclick="speak(this)">
-<svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em" height="1em" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-<g>
-<path fill="#231F20" d="M59.998,28.001h-7.999c-2.211,0-4,1.789-4,4s1.789,4,4,4h7.999c2.211,0,4-1.789,4-4   S62.209,28.001,59.998,28.001z"/>
-<path fill="#231F20" d="M49.71,19.466l6.929-4c1.914-1.105,2.57-3.551,1.461-5.465c-1.102-1.914-3.547-2.57-5.46-1.465l-6.93,4   c-1.914,1.105-2.57,3.551-1.461,5.464C45.351,19.915,47.796,20.571,49.71,19.466z"/>
-<path fill="#231F20" d="M56.639,48.535l-6.929-3.999cuser-1.914-1.105-4.355-0.449-5.461,1.464c-1.105,1.914-0.453,4.359,1.461,5.465   l6.93,4c1.913,1.105,4.358,0.449,5.464-1.465S58.553,49.641,56.639,48.535z"/>
-<path fill="#231F20" d="M37.53,0.307c-1.492-0.625-3.211-0.277-4.359,0.867L18.343,16.001H4c-2.211,0-4,1.789-4,4v24   C0,46.211,1.789,48,4,48h14.343l14.828,14.828C33.937,63.594,34.96,64,35.999,64c0.516,0,1.035-0.098,1.531-0.305   c1.496-0.617,2.469-2.078,2.469-3.695V4.001C39.999,2.384,39.026,0.924,37.53,0.307z"/>
-</g>
-</svg>
-</button>
-<label for="response"> Bot </label>
-<p name="response" class="ai_response">${aiResponse.your_response_back_to_user}</p>
-</div > `;
+</div>`;
         document.getElementById('language').value = data.language;
-      } else {
-        console.error("Invalid data or empty response.");
-      }
-    })
-})
-  .catch(error => {
-    console.error("Fetch error:", error);
-  });
-
-}) ();
+      });
